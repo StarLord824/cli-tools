0633d5d5db48492617850c008ffce795bbc80eef
starlordOG <shuklaabhinav824@gmail.com>
2025-09-26 19:42:26 +0530

    getting started with building clis

---
+++ 1-hello-cli/index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+//basic tool to undersatnd how to use the command line, and build on top of it
+
+console.log('My first command line tool!');
+
+//Question1: How is it different from a normal node.js script?
+//Answer: It is a "module" and it is an "executable"
+
+//Question2: How do you run it?
+//Answer: node index.js
+
+//Question3: How do you run it from the command line?
+//Answer: node index.js
+
+//Question4: But normal codes also run from the command line, why is it different?
+//Answer: Because it is a module and it is an executable
+
+//Question5: Explain the difference between a module and an executable
